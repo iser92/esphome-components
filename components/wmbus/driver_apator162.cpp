3e8186790bc9de8a35f7f38c7db22e575f9e207a
iser92 <37134155+iser92@users.noreply.github.com>
2026-03-10 15:15:09 +0100

    Update driver_apator162.cpp

--- components/wmbus/driver_apator162.cpp
+++ components/wmbus/driver_apator162.cpp
@@ -72,7 +72,7 @@ namespace
         vector<uchar> content;
         t->extractPayload(&content);
 
-        map<string,pair<int,DVEntry>> vendor_values;
+        std::map<string,pair<int,DVEntry>> vendor_values;
 
         // The first 8 bytes are error flags and a date time.
         // E.g. 0F005B5996000000 therefore we skip the first 8 bytes.
